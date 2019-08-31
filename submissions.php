95dae232f26c6d93417cc3fa471d172302111e4a
Jordan Grogan <jordangrogan11@gmail.com>
2019-08-30 20:16:31 -0400

    Send user a text with group details

--- submissions.php
+++ submissions.php
@@ -1,4 +1,8 @@
 <?php
+require 'config.php';
+require 'Twilio/autoload.php';
+
+/* EMAIL TO SMALL GROUPS TEAM */
 $name = htmlspecialchars(stripslashes(trim($_POST['name'])));
 $group = htmlspecialchars(stripslashes(trim($_POST['group'])));
 $email = htmlspecialchars(stripslashes(trim($_POST['email'])));
@@ -10,8 +14,19 @@ $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 $headers .= 'From: "Crossroads Web Team" <webteam@xr.church>' . "\r\n";
 
-// Send email to small group team
-if(mail($to, $subject, $body, $headers)) {
+/* TEXT TO USER */
+$message =  "Here are the small group details:\n" . $group;
+$client = new Twilio\Rest\Client(TWILIO_SID, TWILIO_TOKEN);
+$message = $client->messages->create(
+    $phone, // Text this number
+    array(
+        'from' => TWILIO_PHONE_NUMBER, // From a valid Twilio number
+        'body' => $message
+    )
+);
+
+/* Send email to small group team */
+if (mail($to, $subject, $body, $headers)) {
     http_response_code(200);
 } else {
     http_response_code(400);
